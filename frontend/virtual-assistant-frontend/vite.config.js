a247328aa69b40e20b08573d55b986e0ea93dd87
toshantarora <toshantarora19@gmail.com>
2026-01-25 00:04:53 +0530

    fix issues

--- frontend/virtual-assistant-frontend/vite.config.js
+++ frontend/virtual-assistant-frontend/vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-    server: {
-        port: 3000,
-    },
-  plugins: [ tailwindcss(),react()],
+  server: {
+    port: 3000,
+  },
+  plugins: [tailwindcss(), react()],
   build: {
-    outDir: "dist"
-  }
-})
+    outDir: "dist",
+  },
+});
