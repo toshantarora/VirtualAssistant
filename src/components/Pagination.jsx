a2335f48ec76994a7b770109331f05c7665e0999
SiddhantDeval <siddhantdeval15@gmail.com>
2026-01-29 11:59:16 +0530

    feat: render pagination for single pages and dynamically style navigation buttons based on their disabled state.

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,6 @@
 const Pagination = ({ currentPage, totalItems, itemsPerPage, onPageChange }) => {
   const totalPages = Math.ceil(totalItems / itemsPerPage);
-  if (totalPages <= 1) return null;
+
   return (
     <div className="flex items-center justify-between pt-6 text-sm gap-6">
       <p className="text-green-800">{`Page ${currentPage} of ${totalPages}`}</p>
@@ -9,15 +9,15 @@ const Pagination = ({ currentPage, totalItems, itemsPerPage, onPageChange }) => 
         <button
           disabled={currentPage === 1}
           onClick={() => onPageChange(currentPage - 1)}
-          className="rounded-full border px-4 py-1 text-gray-400"
-        >
+          className={`rounded-full border px-4 py-1 text-gray-400 ${currentPage === 1 ? 'text-gray-400' : 'text-green-700'}`}
+        >   
           Previous
         </button>
 
         <button
           disabled={currentPage === totalPages}
           onClick={() => onPageChange(currentPage + 1)}
-          className="rounded-full border border-green-700 px-4 py-1 text-green-700"
+          className={`rounded-full border px-4 py-1 text-gray-400 ${currentPage === totalPages ? 'text-gray-400' : 'text-green-700'}`}
         >
           Next
         </button>
