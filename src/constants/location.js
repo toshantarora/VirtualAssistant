bf3022864749b22fe7bb91afe27d10c4164de969
SiddhantDeval <siddhantdeval15@gmail.com>
2026-02-12 00:23:39 +0530

    add country uuid of seeded data

--- src/constants/location.js
+++ src/constants/location.js
@@ -1,1 +1,1 @@
-export const DEFAULT_COUNTRY_ID = import.meta.env.VITE_DEFAULT_COUNTRY_ID;
+export const DEFAULT_COUNTRY_ID = import.meta.env.VITE_DEFAULT_COUNTRY_ID || 'ddb25e7d-e3fc-4d44-bf73-d2a23793c8b7';
